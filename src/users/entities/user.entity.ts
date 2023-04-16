a1c8845af5db60fd2b8dd0a7590a61e631b06b87
halfpast-learn <28054220+halfpast-learn@users.noreply.github.com>
2023-04-16 04:26:19 +0300

    Remove unused columns

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -11,14 +11,6 @@ export class User {
     username: string;
     @Column({nullable: true})
     password: string;
-    @Column({nullable: true})
-    first_name: string;
-    @Column({nullable: true})
-    last_name: string;
-    @Column({nullable: true})
-    phone: string;
-    @Column({nullable: true})
-    email: string;
 
     @Column({ nullable: true })
     role: number;
